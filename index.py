a266f03b744c8c92964956798ea96a2502c147d1
Areeba721 <areebaqamar46@gmail.com>
2025-04-13 22:37:36 +0500

    Add files via upload

---
+++ index.py
@@ -0,0 +1,27 @@
+# print
+print("Hello, World!")
+
+# list
+thislist = ["apple", "orange", "mango"]
+print(thislist)
+
+# data types
+# int
+x = 8
+print(type(x))
+# string
+a = "Areeba"
+print(a)
+# list
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+# float
+y = 13.5
+print(type(y))
+# bool
+x = False
+print(type(x))
+
+# tuple
+thistuple = ("car", "tree", "home")
+print(thistuple[-1])
