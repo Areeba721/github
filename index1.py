a4ceadf9f64ef409c1eceb6c5407201438fd525f
Areeba721 <areebaqamar46@gmail.com>
2025-04-22 19:38:08 +0500

    Add files via upload

---
+++ index1.py
@@ -0,0 +1,33 @@
+names = ["Alice", "Bob", "Charlie", "Diana"] 
+for name in names:
+    print(name)
+
+# example 2 (for loop)
+for i in range(1, 6):
+    print(i)
+
+#if statement
+num = 6
+
+if num % 2 == 0:
+    print("Even number")
+
+#nested if
+num = 8
+
+if num > 0:
+    if num % 2 == 0:
+        print("Positive and even")
+
+#whileloop
+i = 1
+
+while i <= 5:
+    print(i)
+    i += 1
+
+# for each loop
+    numbers = [1, 2, 3]
+
+for num in numbers:
+    print(num * 2)
